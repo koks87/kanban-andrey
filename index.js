b20cee4e8942267d50ffa2fc06c7038f474ab1a7
koks87 <ford-s@rambler.ru>
2022-05-13 16:45:54 +0300

    initial comit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+let cards = [
+    { id: '1', name: 'First Cards', status: 'todo', priority: 3 },
+    { id: '2', name: 'Second Cards', status: 'progress', priority: 5 },
+    { id: '3', name: 'Next Cards', status: 'review', priority: 10 },
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/card', (req, res) => {
+    res.send(cards);
+})
+
+app.delete('/card/:cardId', (req, res) => {
+    const cardId = req.params.cardId;
+    cards = cards.filter(el => el.id !== cardId);
+    res.send(cards);
+})
+
+app.post('/card', (req, res) => {
+    const card = req.body;
+    cards.push({id: String(Math.floor(Math.random() * 10000001)),...card});
+    res.send('Card create');
+})
+
+app.patch('/card/:cardId', (req, res) => {
+    const cardId = req.params.cardId;
+    const card = req.body;
+    cards = cards.map(el => el.id === cardId ? ({...card, id:el.id}) : el);
+    res.send('Card updated');
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
