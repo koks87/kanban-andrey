d14803cf9837c01dca81d9461ab80dc2b2796036
koks87 <ford-s@rambler.ru>
2022-05-13 17:46:27 +0300

    test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 
 let cards = [
